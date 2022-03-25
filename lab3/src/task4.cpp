843bac0fce51b0bea8889d68370d3bac7c068492
a.pestov <andru.pestov@gmail.com>
2022-03-25 15:11:43 +0300

    update 3.4

--- lab3/src/task4.cpp
+++ lab3/src/task4.cpp
@@ -5,39 +5,42 @@ using namespace cv;
 using namespace std;
 
 int main(int argc, char** argv){
-	string fn;
-	if (argc>1) fn= argv[1];
-	else fn = "../img_zadan/gk/gk_tmplt.jpg";
-    Mat templ = imread(fn, IMREAD_GRAYSCALE);
-    Mat templ_th;
-    threshold(templ, templ_th, 200, 255, THRESH_BINARY);
-    vector<vector<Point>> cnts_template;
-    findContours(templ_th, cnts_template, RETR_LIST, CHAIN_APPROX_NONE);
+    std::string image_file;
+	if (argc>1) image_file= argv[1];
+	else image_file = "../images/lab3/gk/gk.jpg";
+	Mat image = cv::imread(image_file,IMREAD_COLOR);
+    Mat gray;
+    cvtColor(image, gray,CV_BGR2GRAY);
+    Mat templ = imread("../images/lab3/gk/gk_tmplt.jpg", IMREAD_GRAYSCALE);
+    Mat templ_tresh;
+    Mat finded_temlp = image.clone();
     Mat draw = Mat::zeros(templ.size(), CV_8UC3);
+    Mat gray_tresh, opened, closed, dilated;
+    
+    //ищу контур образца
+    threshold(templ, templ_tresh, 200, 255, THRESH_BINARY);
+    vector<vector<Point>> cnts_template;    
+    findContours(templ_tresh, cnts_template, RETR_LIST, CHAIN_APPROX_NONE);
     drawContours(draw, cnts_template, 0, Scalar(255, 0, 0));
-    imshow("draw", draw);
 
-    Mat img = imread("../img_zadan/gk/gk.jpg", IMREAD_GRAYSCALE);
-    Mat gray, opened, closed, dilated;
-    threshold(img, gray, 230, 255, THRESH_BINARY);
-    imshow("img", gray);
-    morphologyEx(gray, opened, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(3, 3)));
+    // ищу все контуры 
+    threshold(gray, gray_tresh, 230, 255, THRESH_BINARY);
+    morphologyEx(gray_tresh, opened, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(3, 3)));
     morphologyEx(opened, closed, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(3, 3)));
-    imshow("morph", closed);
     vector<vector<Point>> cnts;
-    Mat wrench_draw = imread("../img_zadan/gk/gk.jpg");
     findContours(closed, cnts, RETR_LIST, CHAIN_APPROX_NONE);
+
     for (size_t i = 0; i < cnts.size(); i++)
     {
-        if (contourArea(cnts[i])>1000000)
+        if (contourArea(cnts[i])>99999)
             continue;
         double diff = matchShapes(cnts[i], cnts_template[0], CV_CONTOURS_MATCH_I2, 0);
         if (diff < 1){
-            drawContours(wrench_draw, cnts, i, Scalar(0, 255, 0), 5);  
+            drawContours(finded_temlp, cnts, i, Scalar(0, 255, 0), 5);  
         }
         else
-            drawContours(wrench_draw, cnts, i, Scalar(0, 0, 255), 5);  
+            drawContours(finded_temlp, cnts, i, Scalar(0, 0, 255), 5);  
     }
-    imshow("wrench_draw", wrench_draw);
+    imshow("finded_temlp", finded_temlp);
     waitKey();
 }
